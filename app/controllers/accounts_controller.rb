0c1efd9b836c775a768f17bba638af585225b389
ShoyaUeno1026 <ueno@hab-co.jp>
2024-05-17 11:09:49 +0900

    ユーザーは、アカウントの編集ができる #50

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,6 @@
 class AccountsController < Accounts::BaseController
-  before_action :set_account, only: %i[show]
+  before_action :set_account, only: %i[show edit update destroy]
+  before_action :require_account_admin, only: %i[edit update destroy]
 
   def index
     @accounts = current_user.accounts.order(:created_at)
@@ -9,6 +10,14 @@ class AccountsController < Accounts::BaseController
   end
 
   def edit
+  end
+
+  def update
+    if @account.update(account_params)
+      redirect_to account_url(@account), notice: t(".updated")
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
