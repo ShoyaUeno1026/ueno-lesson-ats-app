29edb03ff1ff7b63e489a0070a55395b6f27bee8
ShoyaUeno1026 <ueno@hab-co.jp>
2024-05-30 14:58:27 +0900

    ユーザー登録の際、アカウントが2つ作成されてしまう不具合の修正 #89

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ApplicationRecord
   has_many :departments, class_name: "Department", foreign_key: :owner_id, inverse_of: :owner, dependent: :nullify
   has_many :jobs, class_name: "Job", foreign_key: :owner_id, inverse_of: :owner, dependent: :nullify
   has_many :candidates, class_name: "Candidate", foreign_key: :owner_id, inverse_of: :owner, dependent: :nullify
-  has_many :accounts, through: :account_users
 
   before_create :skip_confirmation!
   
