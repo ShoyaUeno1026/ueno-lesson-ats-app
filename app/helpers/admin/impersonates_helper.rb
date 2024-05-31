2de3187702b297f6a577ec066a4e6c94e1f50f10
ShoyaUeno1026 <ueno@hab-co.jp>
2024-05-31 12:36:22 +0900

    レイアウト修正

--- app/helpers/admin/impersonates_helper.rb
+++
@@ -1,2 +0,0 @@
-module Admin::ImpersonatesHelper
-end
--- app/models/account_user.rb
+++ app/models/account_user.rb
@@ -4,11 +4,25 @@ class AccountUser < ApplicationRecord
   belongs_to :account
   belongs_to :user
 
+  validates :role, inclusion: {in: ROLES}
+  validates :user_id, uniqueness: {scope: :account_id}
+  validate :owner_must_be_admin, on: :update, if: -> { role_changed? && account_owner? }
+
   def account_owner?
     account.owner_id == user_id
   end
 
   def admin?
     role == "admin"
+  end
+
+  def member?
+    role == "member"
+  end
+
+  def owner_must_be_admin
+    unless admin?
+      errors.add :role, :cannot_be_changed
+    end
   end
 end
